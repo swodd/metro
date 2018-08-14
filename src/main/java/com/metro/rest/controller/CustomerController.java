c63fa007a973d50de49ae2843f76bfd21b36d146
swodd <swodd@mail.ru>
2018-08-15 02:22:18 +0300

    add response status

--- src/main/java/com/metro/rest/controller/CustomerController.java
+++ src/main/java/com/metro/rest/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.metro.rest.controller;
 import com.metro.rest.entity.Customer;
 import com.metro.rest.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,13 +14,15 @@ public class CustomerController {
     CustomerRepository customerRepository;
 
     @PostMapping("/customer")
-    public Customer createCustomer(@RequestBody Customer customer){
-        return customerRepository.save(customer);
+    public ResponseEntity<String> createCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
     }
 
     @GetMapping("/customer/{mobileNo}")
-    public Customer getCustomerById(@PathVariable Long mobileNo){
+    public ResponseEntity<Object> getCustomerById(@PathVariable Long mobileNo){
         System.out.println(customerRepository.findByMobileNo(mobileNo));
-            return customerRepository.findByMobileNo(mobileNo);
+            Customer customer = customerRepository.findByMobileNo(mobileNo);
+            return new ResponseEntity<>(customer, HttpStatus.CREATED);
     }
 }
